be50df0f2f9250b1dd463e3335356c2430772e73
Glenn Downing <downing@cs.utexas.edu>
2014-06-16 09:48:23 -0500

    another commit

--- examples/Types.js
+++ examples/Types.js
@@ -12,15 +12,6 @@ function assert (b) {
 
 println("Types.js");
 
-assert(Object   instanceof Object);
-assert(Function instanceof Function);
-
-assert(Object   instanceof Function);
-assert(Function instanceof Object);
-
-assert(Object.getPrototypeOf(Function.prototype) === Object.prototype);
-assert(Object.getPrototypeOf(Object.prototype)   === null);
-
 var b = false;
 b = true;
 assert((typeof b) === "boolean");
@@ -28,7 +19,6 @@ b = new Boolean(false);
 assert((typeof b) === "object");
 assert(b       instanceof Boolean);
 assert(b       instanceof Object);
-assert(Boolean instanceof Function);
 assert(Boolean instanceof Object);
 assert(Object.getPrototypeOf(b)                 === Boolean.prototype);
 assert(Object.getPrototypeOf(Boolean.prototype) === Object.prototype);
@@ -39,7 +29,6 @@ n = new Number(2);
 assert((typeof n) === "object");
 assert(n      instanceof Number);
 assert(n      instanceof Object);
-assert(Number instanceof Function);
 assert(Number instanceof Object);
 assert(Object.getPrototypeOf(n)                === Number.prototype);
 assert(Object.getPrototypeOf(Number.prototype) === Object.prototype);
@@ -50,7 +39,6 @@ s = new String("abc");
 assert((typeof s) === "object");
 assert(s      instanceof String);
 assert(s      instanceof Object);
-assert(String instanceof Function);
 assert(String instanceof Object);
 assert(Object.getPrototypeOf(s)                === String.prototype);
 assert(Object.getPrototypeOf(String.prototype) === Object.prototype);
@@ -59,23 +47,18 @@ var a = [2, 3, 4];
 assert((typeof a) === "object");
 assert(a     instanceof Array);
 assert(a     instanceof Object);
-assert(Array instanceof Function);
 assert(Array instanceof Object);
 assert(Object.getPrototypeOf(a)               === Array.prototype);
 assert(Object.getPrototypeOf(Array.prototype) === Object.prototype);
 
 var x = {n : 2.34, s : "abc", u : {v : [2, 3, 4]}};
 assert((typeof x) === "object");
-assert(x      instanceof Object);
-assert(Object instanceof Function);
-assert(Object instanceof Object);
+assert(x instanceof Object);
 assert(Object.getPrototypeOf(x) === Object.prototype);
 
 var y = Object.create(x);
 assert((typeof y) === "object");
-assert(y      instanceof Object);
-assert(Object instanceof Function);
-assert(Object instanceof Object);
+assert(y instanceof Object);
 assert(Object.getPrototypeOf(y) === x);
 
 function inc (v) {
@@ -84,7 +67,9 @@ function inc (v) {
 assert((typeof inc) === "function");
 assert(inc      instanceof Function);
 assert(inc      instanceof Object);
-assert(Function instanceof Function);
 assert(Function instanceof Object);
+
+assert(Object instanceof Object);
+assert(Object.getPrototypeOf(Object.prototype) === null);
 
 println("Done.");
